1030b714be1729657c29e596ded393b630fee9b9
mkdesilva <mihindu.desilva@gmail.com>
2019-05-15 19:18:39 +0700

    - Added initial XCode project.

---
+++ MobileBuyersGuide/MobileBuyersGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileBuyersGuide
+//
+//  Created by Mihindu de Silva on 15/05/2019.
+//  Copyright © 2019 Mihindu de Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
