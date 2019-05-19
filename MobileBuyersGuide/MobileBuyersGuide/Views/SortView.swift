b3f89f7323aa2673163c5dfa7a95d604f89e1a5a
mkdesilva <mihindu.desilva@gmail.com>
2019-05-19 19:54:03 +0700

    Added a bold font to the current sort option in SortView

--- MobileBuyersGuide/MobileBuyersGuide/Views/SortView.swift
+++ MobileBuyersGuide/MobileBuyersGuide/Views/SortView.swift
@@ -10,22 +10,56 @@ import UIKit
 
 class SortView: UIView {
     
-    @IBAction func didTapPriceLowToHigh(_ sender: Any) {
-         delegate?.didTapSortPriceLowToHigh()
+    @IBAction func didTapPriceLowToHigh(_ sender: UIButton) {
+        boldenSortOption(sender)
+        delegate?.didTapSortPriceLowToHigh()
     }
     
-    @IBAction func didTapPriceHighToLow(_ sender: Any) {
+    @IBAction func didTapPriceHighToLow(_ sender: UIButton) {
+        boldenSortOption(sender)
         delegate?.didTapSortPriceHighToLow()
     }
     
-    @IBAction func didTapRatingHighToLow(_ sender: Any) {
+    @IBAction func didTapRatingHighToLow(_ sender: UIButton) {
+        boldenSortOption(sender)
         delegate?.didTapSortRatingHighToLow()
     }
     
-    @IBAction func didTapCancel(_ sender: Any) {
-         delegate?.didTapSortCancel()
+    @IBAction func didTapCancel(_ sender: UIButton) {
+        delegate?.didTapSortCancel()
     }
     
+    @IBOutlet weak var ratingHighToLowButton : UIButton!
+    
     weak var delegate : MobileListDelegate?
-
+    
+    var currentBoldButton : UIButton?
+    
+    // Sets the current bold button to default rating button and boldens it
+    func configure() {
+        currentBoldButton = ratingHighToLowButton
+        boldenSortOption(currentBoldButton!)
+    }
+    
+    // Un-applies bold of the currentBoldButton
+    // Applies bold font to newButton and sets it to currentBoldButton
+    fileprivate func boldenSortOption(_ button : UIButton) {
+        guard let oldBoldButton = currentBoldButton else { return }
+        
+        guard let oldTitleLabel = oldBoldButton.titleLabel else {
+            print("Missing title label on sort button. Please fix")
+            return
+        }
+        
+        guard let newTitleLabel = button.titleLabel else {
+            print("Missing title label on sort button. Please fix")
+            return
+        }
+        
+        oldTitleLabel.font = UIFont.systemFont(ofSize: 15)
+        newTitleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        currentBoldButton = button
+    }
+    
 }
